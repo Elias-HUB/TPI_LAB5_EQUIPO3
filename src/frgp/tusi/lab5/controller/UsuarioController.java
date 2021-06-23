d54a6a5dac462c117d70f1d7857e3e1720c00ba8
Elias_Valenzuela <elias.valenzuela@alumnos.frgp.edu.ar>
2021-06-22 21:00:50 -0300

    Manejo de inicio de session

--- src/frgp/tusi/lab5/controller/UsuarioController.java
+++ src/frgp/tusi/lab5/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package frgp.tusi.lab5.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,13 +12,6 @@ public class UsuarioController {
 	@RequestMapping("altaUsuario")
 	public ModelAndView altaUsuario() {
 		ModelAndView mv = new ModelAndView();
-		return mv;
-	}
-	
-	@RequestMapping("inicioSessionUsuario")
-	public ModelAndView inicioSessionUsuario() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("movimiento");
 		return mv;
 	}
 	
@@ -37,5 +32,28 @@ public class UsuarioController {
 		ModelAndView mv = new ModelAndView();
 		return mv;
 	}
+	
+	@RequestMapping("inicioSessionUsuario")
+	public ModelAndView inicioSessionUsuario(String user) {
+		ModelAndView mv = new ModelAndView();
+		if(user.toLowerCase().equals("admin")) {
+			mv.setViewName("cliente");
+			mv.addObject("user", user.toLowerCase());
+		}
+		else if (user.toLowerCase().equals("cliente")) {
+			mv.setViewName("resumen");
+			mv.addObject("user", user.toLowerCase());
+		}
+		else{
+			mv.setViewName("login");
+		}
+		return mv;
+	}
 
+	@RequestMapping("login")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login");
+		return mv;
+	}
 }
