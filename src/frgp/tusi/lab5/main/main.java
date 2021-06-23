fb7cf1c1e519a069bf374073069a9acf63ca1084
ezequiel <ezequielmatiascordoba@gmail.com>
2021-06-22 23:51:30 -0300

    Entrega Final

--- src/frgp/tusi/lab5/main/main.java
+++ src/frgp/tusi/lab5/main/main.java
@@ -1,7 +1,6 @@
 package frgp.tusi.lab5.main;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,9 +10,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-import java.util.List;
 import frgp.tusi.lab5.model.Cliente;
 import frgp.tusi.lab5.model.Cuenta;
+import frgp.tusi.lab5.model.Empleado;
+import frgp.tusi.lab5.model.Movimiento;
+import frgp.tusi.lab5.model.Transferencia;
 import frgp.tusi.lab5.model.Usuario;
 
 public class main {
@@ -38,11 +39,11 @@ public class main {
         
         //////////////////////////////////////////////
     	
-    	List<Cuenta> cuentasCliente1 = new ArrayList<Cuenta>();
-    	List<Cuenta> cuentasCliente2 = new ArrayList<Cuenta>();
-    	List<Cuenta> cuentasCliente3 = new ArrayList<Cuenta>();
-    	List<Cuenta> cuentasCliente4 = new ArrayList<Cuenta>();
-    	List<Cuenta> cuentasCliente5 = new ArrayList<Cuenta>();
+//    	List<Cuenta> cuentasCliente1 = new ArrayList<Cuenta>();
+//    	List<Cuenta> cuentasCliente2 = new ArrayList<Cuenta>();
+//    	List<Cuenta> cuentasCliente3 = new ArrayList<Cuenta>();
+//    	List<Cuenta> cuentasCliente4 = new ArrayList<Cuenta>();
+//    	List<Cuenta> cuentasCliente5 = new ArrayList<Cuenta>();
     	    	
         Cuenta cu = new Cuenta();
     	cu.setCbu(5335235);
@@ -53,7 +54,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente1.add(cu);
+//    	cuentasCliente1.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(45728643);
@@ -64,7 +66,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);   	
-    	cuentasCliente1.add(cu);
+//    	cuentasCliente1.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(75427542);
@@ -75,7 +78,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente2.add(cu);
+//    	cuentasCliente2.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(6436343);
@@ -86,7 +90,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente3.add(cu);
+//    	cuentasCliente3.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(6436434);
@@ -97,7 +102,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente4.add(cu);
+//    	cuentasCliente4.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(2362363);
@@ -108,7 +114,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente4.add(cu);
+//    	cuentasCliente4.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(54754562);
@@ -119,7 +126,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente4.add(cu);
+//    	cuentasCliente4.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(23452346);
@@ -130,7 +138,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente4.add(cu);
+//    	cuentasCliente4.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(6345434);
@@ -141,7 +150,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente5.add(cu);
+//    	cuentasCliente5.add(cu);
+    	session.save(cu);
     	
     	cu = new Cuenta();
     	cu.setCbu(2323541);
@@ -152,7 +162,8 @@ public class main {
     	cu.setEstado(true);
     	cu.setFechaAlta(formattedDate);
     	cu.setFechaUltimaModificacion(formattedDate);
-    	cuentasCliente5.add(cu);
+//    	cuentasCliente5.add(cu);
+    	session.save(cu);
     	
     	Cliente cli = new Cliente();
     	cli.setApellido("Arias");
@@ -162,7 +173,7 @@ public class main {
     	cli.setNacionalidad("Argentina");
     	cli.setFechaNacimiento("14/02/1980");
     	cli.setDomicilio("Java 949");    	
-    	cli.setCuentas(cuentasCliente1);
+//    	cli.setCuentas(cuentasCliente1);
     	session.save(cli);
     	
     	cli = new Cliente();
@@ -173,7 +184,7 @@ public class main {
     	cli.setNacionalidad("Argentina");
     	cli.setFechaNacimiento("10/09/1998");
     	cli.setDomicilio("Netcore 330");
-    	cli.setCuentas(cuentasCliente2);
+//    	cli.setCuentas(cuentasCliente2);
     	session.save(cli);
     	
     	cli = new Cliente();
@@ -184,7 +195,7 @@ public class main {
     	cli.setNacionalidad("Argentina");
     	cli.setFechaNacimiento("02/11/1944");
     	cli.setDomicilio("Javascript 99");
-    	cli.setCuentas(cuentasCliente3);
+//    	cli.setCuentas(cuentasCliente3);
     	session.save(cli);
     	
     	cli = new Cliente();
@@ -195,7 +206,7 @@ public class main {
     	cli.setNacionalidad("Paraguaya");
     	cli.setFechaNacimiento("11/08/1990");
     	cli.setDomicilio("Python");
-    	cli.setCuentas(cuentasCliente4);
+//    	cli.setCuentas(cuentasCliente4);
     	session.save(cli);
     	
     	cli = new Cliente();
@@ -206,7 +217,7 @@ public class main {
     	cli.setNacionalidad("Argentina");
     	cli.setFechaNacimiento("01/01/1980");
     	cli.setDomicilio("Javascript 533");
-    	cli.setCuentas(cuentasCliente5);
+//    	cli.setCuentas(cuentasCliente5);
     	session.save(cli);
     	
     	cli = new Cliente();
@@ -259,24 +270,428 @@ public class main {
     	cli.setDomicilio("");
     	session.save(cli);
     	
+    	//////////////////////////////////////////////////    	
+    	Usuario usu = new Usuario();
+    	Empleado em = new Empleado();
+    	em.setLegajo(1);
+    	em.setApellido("Valenzuela");
+    	em.setNombre("Elias");
+    	em.setDni(40379338);
+    	em.setDomicilio("Fagnano 2784");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Ar");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("EliasValenzuela");
+////    	usu.setPass("ab1234");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(2);
+    	em.setApellido("Suarez");
+    	em.setNombre("Yamil");
+    	em.setDni(14315648);
+    	em.setDomicilio("Marco sastre");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("YamilSuarez");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(3);
+    	em.setApellido("Oriana");
+    	em.setNombre("Garcia");
+    	em.setDni(40379564);
+    	em.setDomicilio("Colombia 1550");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("F");
+////    	usu.setEstado(true);
+//    	usu.setUserName("OrianaGarcia");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(4);
+    	em.setApellido("Oscar");
+    	em.setNombre("Ditolbi");
+    	em.setDni(12546514);
+    	em.setDomicilio("Fagnano 2784");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("OscarDitolbi");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(5);
+    	em.setApellido("Maria");
+    	em.setNombre("Palacios");
+    	em.setDni(1654879);
+    	em.setDomicilio("Elizade 2936");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("F");
+////    	usu.setEstado(true);
+//    	usu.setUserName("MariaPalacios");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(6);
+    	em.setApellido("Sonia");
+    	em.setNombre("Palacios");
+    	em.setDni(12345678);
+    	em.setDomicilio("Elizalde 1564");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("F");
+////    	usu.setEstado(true);
+//    	usu.setUserName("SoniaPalacios");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(7);
+    	em.setApellido("Rodriguez");
+    	em.setNombre("Pablo");
+    	em.setDni(16549134);
+    	em.setDomicilio("Talar 4678");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("");
+////    	usu.setEstado(true);
+//    	usu.setUserName("RodriguezPablo");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(8);
+    	em.setApellido("Contento");
+    	em.setNombre("Juan");
+    	em.setDni(39546514);
+    	em.setDomicilio("Riobamba 1234");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("ContentoJuan");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(9);
+    	em.setApellido("Valverde");
+    	em.setNombre("Roman");
+    	em.setDni(12456879);
+    	em.setDomicilio("Talar 154");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("ValverdeRoman");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	
+    	usu = new Usuario();
+    	em = new Empleado();
+    	em.setLegajo(10);
+    	em.setApellido("Camporo");
+    	em.setNombre("Luciano");
+    	em.setDni(45613598);
+    	em.setDomicilio("Fagnano 65798");
+    	em.setEstado(true);
+    	em.setFechaNacimiento(formattedDate);
+    	em.setNacionalidad("Argentina");
+    	em.setSexo("M");
+////    	usu.setEstado(true);
+//    	usu.setUserName("CamporoLuciano");
+////    	usu.setPass("");
+////    	usu.setIdTipoUsuario(1);
+//    	em.setUsuario(usu);
+    	session.save(em);
+    	    	
     	//////////////////////////////////////////////////
+    	Movimiento mov = new Movimiento();
+    	mov.setDetalle("Transferencia");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(1);
+    	mov.setIdTipoMovimiento(1);
+    	mov.setImporte(1000);
+    	session.save(mov);
+    	
+    	mov = new Movimiento();
+    	mov.setDetalle("Transferencia");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(1);
+    	mov.setIdTipoMovimiento(1);
+    	mov.setImporte(1000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Crédito");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(3);
+    	mov.setIdTipoMovimiento(2);
+    	mov.setImporte(2000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Débito");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(4);
+    	mov.setIdTipoMovimiento(3);
+    	mov.setImporte(3000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Transferencia");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(5);
+    	mov.setIdTipoMovimiento(1);
+    	mov.setImporte(20000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Pago tarjeta");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(1);
+    	mov.setIdTipoMovimiento(4);
+    	mov.setImporte(12000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Débito");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(2);
+    	mov.setIdTipoMovimiento(3);
+    	mov.setImporte(24000);
+    	session.save(mov);
+		
+		mov = new Movimiento();
+    	mov.setDetalle("Transferencia");
+    	mov.setEstado(true);
+    	mov.setFecha(new Date());
+    	mov.setFechaUltimaModificacion(new Date());
+    	mov.setIdCuenta(1);
+    	mov.setIdTipoMovimiento(1);
+    	mov.setImporte(21000);
+    	session.save(mov);
+    	
+
+    	//////////////////////////////////////////////////
+    	Transferencia tra = new Transferencia();
+    	tra.setIdCuentaOrigen(1);
+    	tra.setIdCuentaDestino(2);
+    	tra.setIdMovOrigen(1);
+    	tra.setIdMovDestino(2);
+    	session.save(tra);
+
+    	tra = new Transferencia();
+    	tra.setIdCuentaOrigen(1);
+    	tra.setIdCuentaDestino(9);
+    	tra.setIdMovOrigen(1);
+    	tra.setIdMovDestino(2);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(2);
+    	tra.setIdCuentaDestino(8);
+    	tra.setIdMovOrigen(12);
+    	tra.setIdMovDestino(23);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(3);
+    	tra.setIdCuentaDestino(7);
+    	tra.setIdMovOrigen(14);
+    	tra.setIdMovDestino(52);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(4);
+    	tra.setIdCuentaDestino(6);
+    	tra.setIdMovOrigen(31);
+    	tra.setIdMovDestino(42);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(5);
+    	tra.setIdCuentaDestino(5);
+    	tra.setIdMovOrigen(51);
+    	tra.setIdMovDestino(22);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(6);
+    	tra.setIdCuentaDestino(4);
+    	tra.setIdMovOrigen(31);
+    	tra.setIdMovDestino(42);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(7);
+    	tra.setIdCuentaDestino(3);
+    	tra.setIdMovOrigen(14);
+    	tra.setIdMovDestino(42);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(8);
+    	tra.setIdCuentaDestino(2);
+    	tra.setIdMovOrigen(21);
+    	tra.setIdMovDestino(52);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(9);
+    	tra.setIdCuentaDestino(1);
+    	tra.setIdMovOrigen(21);
+    	tra.setIdMovDestino(52);
+    	session.save(tra);
+		
+		tra = new Transferencia();
+    	tra.setIdCuentaOrigen(1);
+    	tra.setIdCuentaDestino(2);
+    	tra.setIdMovOrigen(11);
+    	tra.setIdMovDestino(12);
+    	session.save(tra);
+    	//////////////////////////////////////////////////
+    	
     	Usuario usuario = new Usuario();
     	usuario.setEstado(true);
-//        String formattedDate = dtf.format(dateObj);
-    	usuario.setFechaAlta(dateObj);
+    	usuario.setFechaAlta(new Date());
     	usuario.setIdTipoUsuario(1);
     	usuario.setPass("1234");
-    	
+    	usuario.setUserName("juancho");
     	session.save(usuario);
-    	
-    	//Se hace un new por cada nuevo usuario que se quiere agregar
+
     	usuario = new Usuario();
     	usuario.setEstado(true);
-    	usuario.setFechaAlta(dateObj);
+    	usuario.setFechaAlta(new Date());
     	usuario.setIdTipoUsuario(1);
-    	usuario.setPass("1234");
-    	
-    	session.save(usuario);    	
+    	usuario.setPass("cliente");
+    	usuario.setUserName("cliente");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(1);
+    	usuario.setPass("54321");
+    	usuario.setUserName("roque");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(1);
+    	usuario.setPass("54321");
+    	usuario.setUserName("robertogomezbolanios");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(1);
+    	usuario.setPass("987654321");
+    	usuario.setUserName("elchavo");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(2);
+    	usuario.setPass("admin");
+    	usuario.setUserName("admin");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(2);
+    	usuario.setPass("nocontaban");
+    	usuario.setUserName("elchapulin");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(2);
+    	usuario.setPass("654321");
+    	usuario.setUserName("doctorchapatin");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(2);
+    	usuario.setPass("123456");
+    	usuario.setUserName("chespirito");
+    	session.save(usuario);
+
+    	usuario = new Usuario();
+    	usuario.setEstado(true);
+    	usuario.setFechaAlta(new Date());
+    	usuario.setIdTipoUsuario(2);
+    	usuario.setPass("123456");
+    	usuario.setUserName("chompiras");
+    	session.save(usuario);
+    	////////////////////////////////////////
     	
     	session.getTransaction().commit();
     	session.close();
